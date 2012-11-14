b9a26fbf3331312f7030d124888520ad18cd1e5a
Andrew Melton <andrew.melton@rackspace.com>
2012-11-14 16:04:04 -0500

    Correctly defining some temporary operations.

--- chronos/api/v1/tasks.py
+++ chronos/api/v1/tasks.py
@@ -8,13 +8,13 @@ class TasksController(object):
     def list_tasks(self, req):
         raise HTTPNotImplemented
 
-    def create_task(self, req):
+    def create_task(self, req, body=None):
         raise HTTPNotImplemented
 
     def get_task(self, req, id):
         raise HTTPNotImplemented
 
-    def update_task(self, req, id):
+    def update_task(self, req, id, body=None):
         raise HTTPNotImplemented
 
     def delete_task(self, req, id):
--- chronos/tests/unit/utils.py
+++ chronos/tests/unit/utils.py
@@ -1,7 +1,9 @@
 from chronos.openstack.common import wsgi
 
-UUID1 = 'c80a1a6c-bd1f-41c5-90ee-81afedb1d58d'
-UUID2 = '971ec09a-8067-4bc8-a91f-ae3557f1c4c7'
+TASK_UUID1 = 'c80a1a6c-bd1f-41c5-90ee-81afedb1d58d'
+TASK_UUID2 = '971ec09a-8067-4bc8-a91f-ae3557f1c4c7'
+
+SERVER_UUID1 = 'ba3f1c5d-89a0-34c1-b44d-ef78d46b90a3'
 
 TENANT1 = '6838eb7b-6ded-434a-882c-b344c77fe8df'
 TENANT2 = '2c014f32-55eb-467d-8fcb-4bd706012f81'
@@ -16,3 +18,14 @@ def get_fake_request(path='', method='GET'):
     req.method = method
 
     return req
+
+
+def get_task(id=None, serverRef=SERVER_UUID1):
+    task = {
+        'task':
+        {
+            'id': id,
+            'serverRef': serverRef
+        }
+    }
+    return task
