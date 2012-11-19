a226a8cf7314e0e6e535bf4474a509d2b08a2be6
Alex Meade <alex.meade@rackspace.com>
2012-11-19 15:01:28 -0500

    Add worker db calls

--- qonos/db/simple/api.py
+++ qonos/db/simple/api.py
@@ -1,3 +1,8 @@
+import uuid
+
+from qonos.openstack.common import timeutils
+
+
 DATA = {
     'schedules': {},
     'schedule_metadata': {},
@@ -10,3 +15,33 @@ def reset():
     global DATA
     for k in DATA:
         DATA[k] = {}
+
+
+def _gen_base_attributes():
+    values = {}
+    values['id'] = str(uuid.uuid4())
+    values['created_at'] = timeutils.isotime()
+    values['updated_at'] = timeutils.isotime()
+    return values
+
+
+def worker_get_all():
+    return DATA['workers'].values()
+
+
+def worker_get_by_id(id):
+    return DATA['workers'][id].copy()
+
+
+def worker_create(values):
+    global DATA
+    worker = {}
+    worker.update(values)
+    worker.update(_gen_base_attributes())
+    DATA['workers'][worker['id']] = worker
+    return worker
+
+
+def worker_delete(id):
+    global DATA
+    del DATA['workers'][id]
