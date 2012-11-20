648a2d6b56c45f759ec54a4b362d891d83d6dc2a
Alex Meade <alex.meade@rackspace.com>
2012-11-20 08:56:30 -0500

    Add job functions to DB

--- qonos/db/simple/api.py
+++ qonos/db/simple/api.py
@@ -51,3 +51,40 @@ def worker_delete(worker_id):
     if worker_id not in DATA['workers']:
         raise exception.NotFound()
     del DATA['workers'][worker_id]
+
+
+def job_create(values):
+    global DATA
+    job = {}
+    job.update(values)
+    job.update(_gen_base_attributes())
+    DATA['jobs'][job['id']] = job
+    return job
+
+
+def job_get_all():
+    return DATA['jobs'].values()
+
+
+def job_get_by_id(job_id):
+    if job_id not in DATA['jobs']:
+        raise exception.NotFound()
+    return DATA['jobs'][job_id].copy()
+
+
+def job_update(job_id, values):
+    global DATA
+    if job_id not in DATA['jobs']:
+        raise exception.NotFound()
+
+    job = DATA['jobs'][job_id]
+    job.update(values)
+    job['updated_at'] = timeutils.isotime()
+    return job
+
+
+def job_delete(job_id):
+    global DATA
+    if job_id not in DATA['jobs']:
+        raise exception.NotFound()
+    del DATA['jobs'][job_id]
