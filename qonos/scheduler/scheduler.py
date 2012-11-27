ce0e579abb086a08b726434d613a4749c7c1f164
Alex Meade <alex.meade@rackspace.com>
2012-11-27 15:17:54 -0500

    Scheduler initial commit

---
+++ qonos/scheduler/scheduler.py
@@ -0,0 +1,16 @@
+from qonos.openstack.common.gettextutils import _
+import qonos.openstack.common.log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+class Scheduler(object):
+    def __init__(self):
+        pass
+
+    def run(self):
+        LOG.debug(_('Starting qonos scheduler service'))
+
+    def get_schedules(self):
+        pass
+
