fec401660f51f1ea5a1919d86150816b28b930ed
Andrew Melton <andrew.melton@rackspace.com>
2012-11-29 15:59:54 -0500

    Recusively serializing datetimes

--- qonos/tests/unit/common/test_utils.py
+++ qonos/tests/unit/common/test_utils.py
@@ -20,6 +20,31 @@ class TestUtils(test_utils.BaseTestCase):
         utils.serialize_datetimes(data)
         self.assertEqual(data, expected)
 
+    def test_serialize_datetimes_list(self):
+        data = {
+            'data': [
+                {'foo': datetime.datetime(2012, 5, 16, 15, 27, 36, 325355)},
+                {'foo': datetime.datetime(2013, 5, 16, 15, 27, 36, 325355)},
+            ]
+        }
+        date_1_str = '2012-05-16T15:27:36Z'
+        date_2_str = '2013-05-16T15:27:36Z'
+        utils.serialize_datetimes(data)
+        self.assertEqual(data['data'][0]['foo'], date_1_str)
+        self.assertEqual(data['data'][1]['foo'], date_2_str)
+
+    def test_serialize_datetimes_nested_dict(self):
+        data = {
+            'data': {
+                'foo': {
+                    'bar': datetime.datetime(2012, 5, 16, 15, 27, 36, 325355)
+                }
+            }
+        }
+        date_1_str = '2012-05-16T15:27:36Z'
+        utils.serialize_datetimes(data)
+        self.assertEqual(data['data']['foo']['bar'], date_1_str)
+
     def test_cron_string_to_datetime(self):
         minute = timeutils.utcnow().minute
         if minute == 0:
