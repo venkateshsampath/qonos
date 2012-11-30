49ce44a0b69ff6f377af50ac12edffe40be6c0f3
Alex Meade <alex.meade@rackspace.com>
2012-11-30 13:34:07 -0500

    Add creation of jobs to scheduler

--- qonos/tests/unit/scheduler/test_scheduler.py
+++ qonos/tests/unit/scheduler/test_scheduler.py
@@ -1,14 +1,20 @@
+import datetime
+import mox
 import time
 
+from qonos.openstack.common import timeutils
 from qonos.scheduler import scheduler
 from qonos.tests import utils as test_utils
+from qonos.tests.unit import utils as unit_utils
 
 
 class TestScheduler(test_utils.BaseTestCase):
 
     def setUp(self):
         super(TestScheduler, self).setUp()
-        self.scheduler = scheduler.Scheduler()
+        self.mox = mox.Mox()
+        self.client = self.mox.CreateMockAnything()
+        self.scheduler = scheduler.Scheduler(self.client)
 
     def tearDown(self):
         super(TestScheduler, self).tearDown()
@@ -47,3 +53,40 @@ class TestScheduler(test_utils.BaseTestCase):
         self.scheduler.run(run_once=True)
         self.assertTrue(called['enqueue_jobs'])
         self.assertTrue(called['log_warn'])
+
+    def test_enqueue_jobs(self):
+        called = {'get_schedules': False}
+
+        def fake(*args, **kwargs):
+            called['get_schedules'] = True
+            return [{'id': unit_utils.SCHEDULE_UUID1}]
+
+        self.stubs.Set(self.scheduler, 'get_schedules', fake)
+        self.client.create_job(mox.IgnoreArg())
+        self.mox.ReplayAll()
+        self.scheduler.enqueue_jobs()
+        self.mox.VerifyAll()
+
+    def test_get_schedules(self):
+        timeutils.set_time_override()
+        previous_run = timeutils.isotime()
+        timeutils.advance_time_seconds(30)
+        current_run = timeutils.isotime()
+
+        filter_args = {'next_run_after': previous_run,
+                       'next_run_before': current_run}
+        self.client.list_schedules(filter_args=filter_args).AndReturn([])
+        self.mox.ReplayAll()
+        self.scheduler.get_schedules(previous_run, current_run)
+        self.mox.VerifyAll()
+
+    def test_get_schedules_no_previous_run(self):
+        current_run = timeutils.isotime()
+
+        epoch = timeutils.isotime(datetime.datetime(1970, 1, 1))
+        filter_args = {'next_run_after': epoch,
+                       'next_run_before': current_run}
+        self.client.list_schedules(filter_args=filter_args).AndReturn([])
+        self.mox.ReplayAll()
+        self.scheduler.get_schedules(current_run=current_run)
+        self.mox.VerifyAll()
