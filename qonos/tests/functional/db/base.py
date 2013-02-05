55a000220f9482f5798412ca4cd28ba9856705a2
eddie-sheffield <eddie.sheffield@rackspace.com>
2013-02-04 22:25:31 -0500

    Fix failing test due to some time sensitivity

--- qonos/tests/functional/db/base.py
+++ qonos/tests/functional/db/base.py
@@ -48,11 +48,14 @@ class TestSchedulesDBApi(test_utils.BaseTestCase):
     def setUp(self):
         super(TestSchedulesDBApi, self).setUp()
         self.db_api = db_api
+        timeutils.set_time_override(datetime.datetime(
+                year=2013, month=1, day=1, hour=9, minute=0))
         self._create_schedules()
 
     def tearDown(self):
         super(TestSchedulesDBApi, self).setUp()
         self.db_api.reset()
+        timeutils.clear_time_override()
 
     def _create_schedules(self):
         fixture = {
