3ea71f7ff62376ef226a1036eaf11a6c7b917f4c
Alex Meade <alex.meade@rackspace.com>
2013-02-18 01:47:11 -0500

    Scheduler to stop backtracking to make jobs

--- qonos/tests/unit/v1/test_api_utils.py
+++ qonos/tests/unit/v1/test_api_utils.py
@@ -14,8 +14,12 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import datetime
+
 from qonos.api.v1 import api_utils
 from qonos.common import exception as exc
+from qonos.common import timeutils
+from qonos.common import utils
 from qonos.tests import utils as test_utils
 
 
@@ -30,3 +34,44 @@ class TestAPIUtils(test_utils.BaseTestCase):
         values = {'name': '1', 'updated_at': '2013-02-02 10:10:23'}
         self.assertRaises(exc.Forbidden,
             api_utils.check_read_only_properties, values)
+
+    def test_schedule_to_next_run(self):
+        timeutils.set_time_override()
+
+        self.called = False
+
+        def fake_next_datetime(min, h, dom, m, dow, start_time):
+            self.called = True
+            self.assertEqual(min, '*')
+            self.assertEqual(h, '*')
+            self.assertEqual(dom, '*')
+            self.assertEqual(m, '*')
+            self.assertEqual(dow, '*')
+            self.assertEqual(timeutils.utcnow(), start_time)
+
+        self.stubs.Set(utils, 'cron_string_to_next_datetime',
+            fake_next_datetime)
+
+        api_utils.schedule_to_next_run({})
+        self.assertTrue(self.called)
+        timeutils.clear_time_override()
+
+    def test_schedule_to_next_run_start_time(self):
+        expected_start_time = timeutils.utcnow() - datetime.timedelta(2)
+
+        self.called = False
+
+        def fake_next_datetime(min, h, dom, m, dow, start_time):
+            self.called = True
+            self.assertEqual(min, '*')
+            self.assertEqual(h, '*')
+            self.assertEqual(dom, '*')
+            self.assertEqual(m, '*')
+            self.assertEqual(dow, '*')
+            self.assertEqual(expected_start_time, start_time)
+
+        self.stubs.Set(utils, 'cron_string_to_next_datetime',
+            fake_next_datetime)
+
+        api_utils.schedule_to_next_run({}, expected_start_time)
+        self.assertTrue(self.called)
